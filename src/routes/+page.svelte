412396bed1eb5170be8849a984684247676624de
Alexis Simonian <alexis.simonian@proton.me>
2025-10-17 14:33:48 +0200

    responsive first page + new linter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,203 +1,212 @@
 <script>
-    import GregButton from "$lib/components/GregButton.svelte";
+  import GregButton from "$lib/components/GregButton.svelte";
 </script>
 
 <header id="main-header">
-    <div id="titleAndCta">
-        <h2 class="no-underline">Grégory Simonian</h2>
-        <GregButton />
-    </div>
+  <div id="titleAndCta">
+    <h2 class="no-underline">Grégory Simonian</h2>
+    <GregButton />
+  </div>
 </header>
 <div class="faux-body">
-    <section>
-        <header>
-            <h2>conteur</h2>
-        </header>
-        <div>
-            <p>
-                Je propose des récits puisés dans le répertoire traditionnel
-                pour le jeune public ou pour les adultes et des ateliers de
-                création ludiques qui vous invitent à donner vie à votre propre
-                conte. Mes spectacles et ateliers inclusifs sont adaptés à tous
-                les publics et à chaque lieu.
-            </p>
-        </div>
-    </section>
-    <section>
-        <p class="poetry-section">
-            Transmis par une parole vivante,<br />
-            Les contes traversent les siècles et les océans,<br />
-            Pour arriver jusqu’à nos oreilles.<br />
-            Qu’ont-ils à nous raconter ?
+  <section>
+    <header>
+      <h2>conteur</h2>
+    </header>
+    <div>
+      <p>
+        Je propose des récits puisés dans le répertoire traditionnel pour le
+        jeune public ou pour les adultes et des ateliers de création ludiques
+        qui vous invitent à donner vie à votre propre conte. Mes spectacles et
+        ateliers inclusifs sont adaptés à tous les publics et à chaque lieu.
+      </p>
+    </div>
+  </section>
+  <section>
+    <p class="poetry-section">
+      Transmis par une parole vivante,<br />
+      Les contes traversent les siècles et les océans,<br />
+      Pour arriver jusqu’à nos oreilles.<br />
+      Qu’ont-ils à nous raconter ?
+    </p>
+  </section>
+  <section>
+    <header>
+      <h2>ce que je propose</h2>
+    </header>
+    <div id="two-offers">
+      <div class="offer-card">
+        <h3>spectacles de contes</h3>
+        <p>
+          Écouter un conte, c'est ouvrir une porte vers des mondes inattendus et
+          s'émerveiller ensemble. Un moment de partage simple et authentique qui
+          nous reconnecte à la saveur des histoires. Des spectacles pour tous
+          les âges, des tout-petits aux très grands.
         </p>
-    </section>
-    <section>
-        <header>
-            <h2>ce que je propose</h2>
-        </header>
-        <div id="two-offers">
-            <div class="offer-card">
-                <h3>spectacles de contes</h3>
-                <p>
-                    Écouter un conte, c'est ouvrir une porte vers des mondes
-                    inattendus et s'émerveiller ensemble. Un moment de partage
-                    simple et authentique qui nous reconnecte à la saveur des
-                    histoires. Des spectacles pour tous les âges, des
-                    tout-petits aux très grands.
-                </p>
-                <div class="card-button">
-                    <GregButton page="spectacles" label="En Savoir Plus" />
-                </div>
-            </div>
-            <div class="offer-card">
-                <h3>ateliers d&#x2019écriture</h3>
-                <p>
-                    Créer des contes nous plonge au cœur de notre imaginaire, où
-                    naissent des histoires drôles, surprenantes et révélatrices.
-                    Atelier accessible à tous, qui s’adapte à chaque
-                    participant. Découvrez les bases de la narration et repartez
-                    avec votre propre conte.
-                </p>
-                <div class="card-button">
-                    <GregButton page="ateliers" label="En Savoir Plus" />
-                </div>
-            </div>
+        <div class="card-button">
+          <GregButton page="spectacles" label="En Savoir Plus" />
         </div>
-    </section>
+      </div>
+      <div class="offer-card">
+        <h3>ateliers d&#x2019écriture</h3>
+        <p>
+          Créer des contes nous plonge au cœur de notre imaginaire, où naissent
+          des histoires drôles, surprenantes et révélatrices. Atelier accessible
+          à tous, qui s’adapte à chaque participant. Découvrez les bases de la
+          narration et repartez avec votre propre conte.
+        </p>
+        <div class="card-button">
+          <GregButton page="ateliers" label="En Savoir Plus" />
+        </div>
+      </div>
+    </div>
+  </section>
 </div>
 
 <style lang="scss">
-    p {
-        max-width: 50rem;
+  p {
+    max-width: 50rem;
+  }
+
+  h2.no-underline {
+    display: block;
+  }
+
+  h2.no-underline::after {
+    display: none;
+  }
+
+  #main-header {
+    background-color: black;
+    background-image: url("https://images.gregoryconte.com/main-header-image-black.png");
+    background-size: 70%;
+    background-repeat: no-repeat;
+    background-position: top right;
+    height: 38rem;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    padding: 0 15%;
+    box-sizing: border-box;
+
+    @media screen and (max-width: $breakpoint-lg) {
+      padding: 0 10%;
     }
 
-    h2.no-underline {
-        display: block;
+    @media screen and (max-width: $breakpoint-md) {
+      justify-content: center;
     }
 
-    h2.no-underline::after {
+    h2 {
+      color: $white-color;
+      font-size: $h2-main-header-lg;
+
+      @media screen and (max-width: $breakpoint-xl) {
+        font-size: $h2-main-header-md;
+      }
+
+      @media screen and (max-width: $breakpoint-md) {
         display: none;
+      }
     }
 
-    #main-header {
-        background-color: black;
-        background-image: url("https://images.gregoryconte.com/main-header-image-black.png");
-        background-size: 70%;
-        background-repeat: no-repeat;
-        background-position: top right;
-        height: 38rem;
-        overflow: hidden;
-        display: flex;
-        align-items: center;
-        padding: 0 15%;
-        box-sizing: border-box;
-
-        @media screen and (max-width: $breakpoint-lg) {
-            padding: 0 10%;
-        }
-
-        @media screen and (max-width: $breakpoint-md) {
-            justify-content: center;
-        }
-
-        h2 {
-            color: $white-color;
-            font-size: $h2-main-header-lg;
-
-            @media screen and (max-width: $breakpoint-xl) {
-                font-size: $h2-main-header-md;
-            }
-
-            @media screen and (max-width: $breakpoint-md) {
-                display: none;
-            }
-        }
-
-        @media screen and (width <= 1810px) {
-            height: 30rem;
-        }
-
-        @media screen and (width <= 1500px) {
-            height: 25rem;
-        }
-
-        @media screen and (width <= 768px) {
-            padding: 0 2rem 2rem 2rem;
-            background-size: 100%;
-            background-position: center;
-        }
+    @media screen and (width <= 1810px) {
+      height: 30rem;
     }
 
-    #titleAndCta {
-        @media screen and (max-width: $breakpoint-md) {
-            display: flex;
-            flex-direction: column;
-            height: 100%;
-            justify-content: flex-end;
-            align-items: center;
-        }
+    @media screen and (width <= 1500px) {
+      height: 25rem;
     }
 
-    .poetry-section {
-        font-size: $p-poetry-lg;
-        font-weight: $bold-lg;
-
-        @media screen and (max-width: $breakpoint-xl) {
-            font-size: $p-poetry-md;
-        }
+    @media screen and (width <= 1200px) {
+      height: 20rem;
     }
 
-    #two-offers {
-        display: flex;
-        text-align: left;
-        max-width: $page-max-width-lg;
-        gap: $two-offers-gap-lg;
-
-        @media screen and (max-width: $breakpoint-xl) {
-            gap: $two-offers-gap-md;
-        }
-
-        @media screen and (max-width: $breakpoint-lg) {
-            gap: $two-offers-gap-s;
-        }
-
-        @media screen and (max-width: $additional-breakpoint-two-offers) {
-            gap: $two-offers-gap-xs;
-        }
-
-        @media screen and (max-width: $grid-breakpoint-two-offers) {
-            display: block;
-            text-align: center;
-            max-width: $content-max-width-md;
-        }
+    @media screen and (width <= 900px) {
+      height: 17rem;
     }
 
-    .offer-card {
-        flex: 1;
-        padding: $two-offers-padding-lg;
-        display: flex;
-        flex-direction: column;
-
-        @media screen and (max-width: $breakpoint-lg) {
-            padding: $two-offers-padding-md;
-        }
-
-        @media screen and (max-width: $breakpoint-s) {
-            padding: $two-offers-padding-s;
-        }
-
-        p {
-            flex-grow: 1;
-            margin-bottom: 2rem;
-        }
-
-        .card-button {
-            margin-top: auto;
-            align-self: flex-start;
-
-            @media screen and (max-width: $grid-breakpoint-two-offers) {
-                align-self: center;
-            }
-        }
+    @media screen and (max-width: $breakpoint-md) {
+      padding: 0 2rem 2rem 2rem;
+      background-size: 120%;
+      background-position: top center;
     }
+
+    @media screen and (width <= 500px) {
+      background-size: 140%;
+    }
+  }
+
+  #titleAndCta {
+    @media screen and (max-width: $breakpoint-md) {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      justify-content: flex-end;
+      align-items: center;
+    }
+  }
+
+  .poetry-section {
+    font-size: $p-poetry-lg;
+    font-weight: $bold-lg;
+
+    @media screen and (max-width: $breakpoint-xl) {
+      font-size: $p-poetry-md;
+    }
+  }
+
+  #two-offers {
+    display: flex;
+    text-align: left;
+    max-width: $page-max-width-lg;
+    gap: $two-offers-gap-lg;
+
+    @media screen and (max-width: $breakpoint-xl) {
+      gap: $two-offers-gap-md;
+    }
+
+    @media screen and (max-width: $breakpoint-lg) {
+      gap: $two-offers-gap-s;
+    }
+
+    @media screen and (max-width: $additional-breakpoint-two-offers) {
+      gap: $two-offers-gap-xs;
+    }
+
+    @media screen and (max-width: $grid-breakpoint-two-offers) {
+      display: block;
+      text-align: center;
+      max-width: $content-max-width-md;
+    }
+  }
+
+  .offer-card {
+    flex: 1;
+    padding: $two-offers-padding-lg;
+    display: flex;
+    flex-direction: column;
+
+    @media screen and (max-width: $breakpoint-lg) {
+      padding: $two-offers-padding-md;
+    }
+
+    @media screen and (max-width: $breakpoint-s) {
+      padding: $two-offers-padding-s;
+    }
+
+    p {
+      flex-grow: 1;
+      margin-bottom: 2rem;
+    }
+
+    .card-button {
+      margin-top: auto;
+      align-self: flex-start;
+
+      @media screen and (max-width: $grid-breakpoint-two-offers) {
+        align-self: center;
+      }
+    }
+  }
 </style>

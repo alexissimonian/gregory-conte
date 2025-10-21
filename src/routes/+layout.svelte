d4c84e337ea6a8a233843157a470c11228b01749
Alexis <alexis.simonian@outlook.com>
2025-10-21 09:16:34 +0000

    added favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 	import '../app.scss';
     import GregNavbar from '$lib/components/GregNavbar.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="manifest" href="/site.webmanifest">
 </svelte:head>
 <GregNavbar/>
 {@render children?.()}

555d5ea6dfa29754636023dee8a4121cae601e2a
Alexis Simonian <alexis.simonian@proton.me>
2025-10-22 11:34:22 +0200

    crsf

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    server: {
-        allowedHosts: ['dev.alexissimonian.net']
-    },
-    css: {
-        preprocessorOptions: {
-            scss: { additionalData: `@use "/src/styles/_variables.scss" as *;`, }
-        }
+  plugins: [sveltekit()],
+  server: {
+    allowedHosts: ['dev.alexissimonian.net', 'gregoryraconte.com']
+  },
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@use "/src/styles/_variables.scss" as *;`, }
     }
+  }
 });
